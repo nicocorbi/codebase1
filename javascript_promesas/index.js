30e601455d1695ab857562c269c967d7f25afa2e
datanico <nicolascorbi2002@gmail.com>
2025-01-14 11:09:48 +0100

    s

--- javascript_promesas/index.js
+++ javascript_promesas/index.js
@@ -4,33 +4,46 @@ fetch('https://dummyjson.com/recipes/1')
 .then(res => res.json())
 .then(data => {
     console.log(data)
-    // titulo receta
+    // título de la receta
     const nombre = data.name
     console.log(data.name)
     const h1 = document.createElement("h1")
     h1.innerText = nombre
-    document.body.appendChild(h1)// introducir en el html
+    document.body.appendChild(h1) // introducir en el HTML
+
     // dificultad 
     const difficulty = data.difficulty
     const dificultadElemento = document.createElement("p")
-    dificultadElemento.innerText = difficulty
+    dificultadElemento.innerText = `Dificultad: ${difficulty}`
     document.body.appendChild(dificultadElemento)
+
     // ingredientes
     const ingredients = data.ingredients
-    const ingredientesElemento = document.createElement("p")
+    const ingredientesElemento = document.createElement("ul") // lista desordenada
     document.body.appendChild(ingredientesElemento)
-    ingredientesElemento.innerText = ingredients
+    ingredients.forEach(ingredient => {
+        const item = document.createElement("li")
+        item.innerText = ingredient
+        ingredientesElemento.appendChild(item)
+    })
+
     // pasos 
     const instructions = data.instructions
-    const instruccionesElemento = document.createElement("p")
+    const instruccionesElemento = document.createElement("ol") // lista ordenada
     document.body.appendChild(instruccionesElemento)
-    instruccionesElemento.innerText = instructions
+    instructions.forEach(step => {
+        const item = document.createElement("li")
+        item.innerText = step
+        instruccionesElemento.appendChild(item)
+    })
+
     // imagen
     const imagenElemento = document.createElement("img");
-     imagenElemento.src = data.image; 
+    imagenElemento.src = data.image; 
     imagenElemento.alt = `Imagen de la receta ${nombre}`;
     imagenElemento.style.width = "50%"; 
     document.body.appendChild(imagenElemento);
+
     // Tiempo de preparación
     const preparacion = data.prepTimeMinutes;
     const preparacionElemento = document.createElement("p");
@@ -42,11 +55,7 @@ fetch('https://dummyjson.com/recipes/1')
     const tiempoCocinadoElemento = document.createElement("p");
     tiempoCocinadoElemento.innerText = `Tiempo de cocción: ${tiempoCocinado} minutos`;
     document.body.appendChild(tiempoCocinadoElemento);
-
-
-
 })
-
 
 
          
