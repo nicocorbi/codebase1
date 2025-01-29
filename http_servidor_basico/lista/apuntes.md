de06aa723585168bb8df44c1587a7f5a9dd8dcde
datanico <nicolascorbi2002@gmail.com>
2025-01-29 11:46:51 +0100

    hola

---
+++ http_servidor_basico/lista/apuntes.md
@@ -0,0 +1,44 @@
+<ul>
+<li>item1</li>
+<li>item2</li>
+<li>item3</li>
+</ul>
+## estructura de una tabla html
+## BeEF para penetrar en el pc de alguien
+<table>
+<thead>
+<tr>
+<td>nombre</td>
+<td>nombre</td>
+<td>nombre</td>
+<td>nombre</td>
+</tr>
+</thead>
+</table>
+## estructura de una tabla JS
+createElement/getElementbyid/querySelector
+innerText
+appendChild - donde quiero inyectar .appendchild(que quiero inyectar)
+document.body.appendChild - es lo mismo que appendChild
+
+
+const users[
+    {
+        nombre:"admin",
+        nombre:"admin",
+        nombre:"admin",
+        nombre:"admin"
+    }
+]
+const table = document.createelement("table")
+const thead = document.createelement("thead")
+table.appendChild(thead)
+const tr = document.createElement("tr")
+// donde_quiero_inyectar.appendChild(que_quiero_inyectar)
+thead
+
+
+// document.body.appendChild(table)
+
+## que es el dom
+es la representacion del documento html en un documento de javascript
---
+++ http_servidor_basico/lista/lista.js
@@ -0,0 +1,47 @@
+const users = [
+    {
+      nombre: "admin",
+      password: "admin",
+      email: "admin@email.com",
+      status: "active"
+    },
+  {
+      nombre: "alberto",
+      password: "maracuya",
+      email: "alberto@email.com",
+      status: "inactive",
+      edad: "24"
+    }
+  ]
+  
+  const table = document.createElement("table")
+  const thead = document.createElement("thead")
+  table.appendChild(thead)
+  const tr = document.createElement("tr")
+  thead.appendChild(tr)
+  //donde_quiero_inyectar.appendChild(que_quiero_inyectar)
+  const claves = Object.keys(users[1])
+  for(let i=0; i < claves.length; i++){
+    console.log(claves[i])
+    const td = document.createElement("td")
+    td.innerText = claves[i]
+    tr.appendChild(td)
+  }
+  
+  const tbody = document.createElement("tbody")
+  table.appendChild(tbody)
+  
+  for(let i=0; i < users.length; i++){
+    console.log(users[i])
+    const tr = document.createElement("tr")
+    for(let j=0; j < claves.length; j++){
+      const td = document.createElement("td")
+      const clave = claves[j]
+      td.innerText = users[i][clave]
+      tr.appendChild(td)
+    }
+    tbody.appendChild(tr)
+  }
+  document.body.appendChild(table)
+  // aislar el codigo que hemos creado , una funcion en javascript 
+  // que dado una array de un objeto lo haga en la tabla.
