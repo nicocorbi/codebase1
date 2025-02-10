efc87923fe04c0e6cb59810f7ca265d7d47af6b8
datanico <nicolascorbi2002@gmail.com>
2025-02-10 12:40:01 +0100

    m

--- sqlite3/app-tareas/tareas.py
+++ sqlite3/app-tareas/tareas.py
@@ -10,3 +10,11 @@ def create_table():
 def add_task(title,status,desc=None):
     c.execute("INSERT INTO tasks(title,description,status) VALUES(?,?,?)",(title,desc,status))
     conn.commit()
+
+#TODO:
+#funcion change_status
+#funcion show_tasks
+#funcion delete_tasks
+
+##hacer para el proximo dia un diagrama desde marmaid usando estas 
+# tabla y implementa las 3 funciones
